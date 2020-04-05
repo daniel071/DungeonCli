8519acf35eed44b9f5a08520d9552cb4922fffe0
Daniel Pavela <daniel.pavela@gmail.com>
2020-04-05 20:33:30 +1000

    Added colours! And some stuff!
    Signed-off-by: Daniel Pavela <daniel.pavela@gmail.com>

--- DungeonCli.py
+++ DungeonCli.py
@@ -1,22 +1,32 @@
 # DungeonCli is a terminal based program where you get to explore places and
 # earn coins. You can spend those coins on various items, have fun!
-print("Hello World")
-
-coins = 0 # fucking poor cunt lmao.
-
+print("Welcome to DungeonCli!")
 
 # Import Libraries here:
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
+# Define variables here:
+# TODO: Add saving mechanic for these coins
+coins = 0 # fucking poor cunt lmao.
+success = Style.BRIGHT + Fore.GREEN + "==> "
 
 
 # Define functions here:
 
 def checkCoins():
-	print("you have $" + str(coins) + "!")
+    global coins
+    print("You have $" + str(coins) + "!")
+    if coins == 0:
+        print("You have 0 coins? I feel bad, here take 10 coins!")
+        coins = coins + 10
+        print(success + "10 coins have been added to your account.")
+
 
 def main():
-	x = input("Action> ")
-	if x in ("check money", "check coins"):
+	command = input(Fore.CYAN + "Action> " + Style.RESET_ALL)
+	if command in ("check money", "check coins", "coins", "c"):
 		checkCoins()
 
 # Run those functions here:
