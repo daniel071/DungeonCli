efb5895ddf443f08c0b39baa61ac95c6dea42ad3
Xenthio <thegooster88@gmail.com>
2020-04-12 14:22:47 +1000

    j

--- compile.sh
+++ compile.sh
@@ -1,3 +1,11 @@
 # Add to compile executables for each OS, so that Python isn't required
 # python DungeonCli.py
-python -m nuitka --follow-imports DungeonCli.py
+mkdir .Builds
+python3 -m nuitka --follow-imports DungeonCli.py
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	mv ./DungeonCli.bin ./.Builds/DungeonCli_osx
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+	mv ./DungeonCli.bin ./.Builds/DungeonCli_linux
+else
+	mv ./DungeonCli.exe ./.Builds/DungeonCli.exe
+fi
