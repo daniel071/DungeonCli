12ba939650b9959152f979ec92842d7340b39896
Xenthio <thegooster88@gmail.com>
2020-04-27 09:41:07 +1000

    restored archive.

---
+++ Archive/compileLinux.sh
@@ -0,0 +1,1 @@
+gcc ./main.c -o ./main_linux && ./main_linux
---
+++ Archive/main.c
@@ -0,0 +1,28 @@
+/*
+DungeonCli is a terminal based program where you get to explore
+places and earn coins. You can spend those coins on various items,
+have fun!
+
+Programming: Daniel, Xenthio
+Dialogue: Bob
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  char input[64];
+  int coins = 0; // Fucking poor cunt lmao.
+
+  char *intro = "you are some faggot who cant program, type a b or c";
+  char[] introchoices = [ "a", "b", "c" ];
+  // This is a comment
+  // I have no idea what I am doing - Daniel who uses Python
+  print(intro);
+  while (1)
+  {
+    printf("input> ");
+    fgets(input, 64, stdin);
+    printf("you entered %s\n", input);
+  }
+}
