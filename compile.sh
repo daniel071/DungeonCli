49631b2ed44be65185fea26e2e96a0324f6ea42f
Xenthio <thegooster88@gmail.com>
2020-04-30 20:35:11 +1000

    FUCKING YESSS IT WORKSSSSSS YESS

--- compile.sh
+++ compile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 mkdir bin.build
-python3 -m nuitka --follow-imports --standalone --plugin-enable=multiprocessing --file-reference-choice=runtime DungeonCli.py
+python3 -m nuitka --standalone --remove-output --output-dir=bin.build --file-reference-choice=runtime DungeonCli.py
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	mv ./DungeonCli.bin ./bin.build/DungeonCli_osx
+	sudo codesign -f -s - ./bin.build/DungeonCli.dist/Python
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
 	mv ./DungeonCli.bin ./bin.build/DungeonCli_linux
 else
 	mv ./DungeonCli.bin ./bin.build/DungeonCli
 fi
+
+cp -r ./Music ./bin.build/DungeonCli.dist/
+cp -r ./Sounds ./bin.build/DungeonCli.dist/
--- soundtest.py
+++ soundtest.py
@@ -2,16 +2,16 @@
 import simpleaudio
 import pydub
 import time
+import os
 
-sound = (pydub.AudioSegment.from_ogg("sounds/explosion.ogg") * 20)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+sound = (pydub.AudioSegment.from_ogg("Sounds/explosion.ogg") * 20)
 playback = simpleaudio.play_buffer(
     sound.raw_data,
     num_channels=sound.channels,
     bytes_per_sample=sound.sample_width,
     sample_rate=sound.frame_rate
 )
-
-# end playback after 3 seconds
 time.sleep(3)
 playback.stop()
