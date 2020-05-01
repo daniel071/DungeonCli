bb1643f5883b3c8db353ee6ac1cf66c88df04023
Xenthio <thegooster88@gmail.com>
2020-05-01 12:52:48 +1000

    Update to the windows compiler batch file, possibly might work now?

--- compilewindows.bat
+++ compilewindows.bat
@@ -1,4 +1,6 @@
 md bin.build
-python -m nuitka --follow-imports --plugin-enable=multiprocessing DungeonCli.py
-move .\DungeonCli.exe .\bin.build\DungeonCli
+python --standalone --remove-output --output-dir=bin.build --file-reference-choice=runtime DungeonCli.py
+copy .\Music .\bin.build\DungeonCli.dist\
+copy .\Sounds .\bin.build\DungeonCli.dist\
+echo the build is in .\bin.build\DungeonCli.dist\
 pause
