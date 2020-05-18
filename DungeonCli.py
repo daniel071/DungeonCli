eac163e02c88bfd2a94f5c1c69034e31cda4f6e9
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-18 13:58:11 +1000

    Fixed spelling mistakes + link

--- DungeonCli.py
+++ DungeonCli.py
@@ -1975,11 +1975,11 @@ if __name__ == '__main__':
 					print(Style.RESET_ALL + "==> Creating directories - 1/3...")
 					os.system('md "c:\\Program Files\\ffmpeg')
 					print(Style.RESET_ALL + "==> Downloading FFMPEG - 2/3...")
-					os.system('certutil.exe -urlcache -split -f "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.2.2-win64-static.zip" "c:\\Program Files\\ffmpeg"')
+					os.system('certutil.exe -urlcache -split -f "http://pavela.net:3000/Daniel/DungeonCli/src/branch/master/ffmpeg/ffmpeg.exe" "c:\\Program Files\\ffmpeg"')
 					print("\n==>Adding FFMPEG to Path - 3/3...")
 					os.system('set PATH=%PATH%;"c:\\Program Files\\ffmpeg\\"')
 
-					printScan(success + "Successfully install FFMPEG!")
+					printScan(success + "Successfully installed FFMPEG!")
 					playSound("Music/intro.ogg", True)
 					askLoop = False
 				else:
