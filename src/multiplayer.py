fab2c5a365696e7f79e74065e4a118fee8e76545
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-27 15:41:59 +1000

    implemented basic messaging in multiplayer

--- src/multiplayer.py
+++ src/multiplayer.py
@@ -1,1 +1,43 @@
-# yet to be made...
+# oh boi here we go
+import sys
+sys.path.append('../')
+
+from colorama import Fore, Back, Style
+from colorama import init
+from PyInquirer import prompt, print_json
+# <<< import DungeonCli >>>
+# idk why this import takes ageeessss
+# maybe because its 2000 lines hmmm...
+
+from rethinkdb import RethinkDB
+
+questions = [
+	{
+		'type': 'input',
+		'message': 'Enter server IP',
+		'name': 'serverIP',
+		'default': 'pavela.net'
+	},
+	{
+		'type': 'input',
+		'message': 'Enter your username',
+		'name': 'username',
+		'default': ''
+	}
+]
+answers = prompt(questions)
+serverIP = answers['serverIP']
+yourUserName = answers['username']
+
+
+r = RethinkDB()
+r.connect(serverIP, 28015).repl()
+
+cursor = r.table("chat").changes().run()
+for document in cursor:
+	username = document['new_val']['username']
+	message = document['new_val']['message']
+
+	print(Style.BRIGHT + Fore.YELLOW + "[{username}]: ".format(username=username)
+	+ Style.RESET_ALL + "{message}".format(message=message))
+	print(document)
