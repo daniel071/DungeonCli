38c2bfc0d6110f0d449b20fb9e6ba67150fb9849
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-27 16:57:39 +1000

    WIP chat now implemented to DungeonCli

--- src/multiplayer.py
+++ src/multiplayer.py
@@ -13,52 +13,53 @@ from PyInquirer import prompt, print_json
 
 from rethinkdb import RethinkDB
 
-questions = [
-	{
-		'type': 'input',
-		'message': 'Enter server IP',
-		'name': 'serverIP',
-		'default': 'pavela.net'
-	},
-	{
-		'type': 'input',
-		'message': 'Enter your username',
-		'name': 'username',
-	}
-]
-answers = prompt(questions)
-serverIP = answers['serverIP']
-yourUserName = answers['username']
-
-
-r1 = RethinkDB()
-r2 = RethinkDB()
-
-def checkMessages():
-	r1.connect(serverIP, 28015).repl()
-	cursor = r1.table("chat").changes().run()
-	for document in cursor:
-		username = document['new_val']['username']
-		message = document['new_val']['message']
-
-		print(Style.BRIGHT + Fore.YELLOW + "[{username}]: ".format(username=username)
-		+ Style.RESET_ALL + "{message}".format(message=message))
-
-
-checkThread = threading.Thread(target=checkMessages)
-checkThread.start()
-# To stop thread: checkThread.join()
-
-questions = [
-	{
-		'type': 'input',
-		'message': '>',
-		'name': 'yourMessage',
-	},
-]
-while True:
+def runMe():
+	questions = [
+		{
+			'type': 'input',
+			'message': 'Enter server IP',
+			'name': 'serverIP',
+			'default': 'pavela.net'
+		},
+		{
+			'type': 'input',
+			'message': 'Enter your username',
+			'name': 'username',
+		}
+	]
 	answers = prompt(questions)
-	yourMessage = answers['yourMessage']
-	sys.stdout.write("\033[F") # Clears the text above
-	r2.connect(serverIP, 28015).repl()
-	r2.table('chat').insert([{"username":yourUserName, "message":yourMessage}]).run()
+	serverIP = answers['serverIP']
+	yourUserName = answers['username']
+
+
+	r1 = RethinkDB()
+	r2 = RethinkDB()
+
+	def checkMessages():
+		r1.connect(serverIP, 28015).repl()
+		cursor = r1.table("chat").changes().run()
+		for document in cursor:
+			username = document['new_val']['username']
+			message = document['new_val']['message']
+
+			print(Style.BRIGHT + Fore.YELLOW + "[{username}]: ".format(username=username)
+			+ Style.RESET_ALL + "{message}".format(message=message))
+
+
+	checkThread = threading.Thread(target=checkMessages)
+	checkThread.start()
+	# To stop thread: checkThread.join()
+
+	questions = [
+		{
+			'type': 'input',
+			'message': '>',
+			'name': 'yourMessage',
+		},
+	]
+	while True:
+		answers = prompt(questions)
+		yourMessage = answers['yourMessage']
+		sys.stdout.write("\033[F") # Clears the text above
+		r2.connect(serverIP, 28015).repl()
+		r2.table('chat').insert([{"username":yourUserName, "message":yourMessage}]).run()
