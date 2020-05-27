e6817d1dc76cb4625f0078c37c2c6897b0e46e22
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-27 10:55:26 +1000

    fixed rich presence being anger

--- src/richPrecense.py
+++ src/richPrecense.py
@@ -2,11 +2,13 @@ from pypresence import Presence
 import time
 
 client_id = '714997150941053028'  # Fake ID, put your real one here
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect() # Start the handshake loop
+
 
 def init():
-	RPC = Presence(client_id)  # Initialize the client class
-	RPC.connect() # Start the handshake loop
 	RPC.update(state="Scene 0", details="A terminal-based dungeon game!", large_text="pavela.net:3000/Daniel/DungeonCli", large_image="epicTerminal.png")  # Set the presence
 
 def present(sceneNumber):
+	global RPC
 	RPC.update(state="Scene {scene}".format(scene=sceneNumber), details="A terminal-based dungeon crawler game!", large_text="pavela.net:3000/Daniel/DungeonCli", 		large_image="epicTerminal.png")  # Set the presence
