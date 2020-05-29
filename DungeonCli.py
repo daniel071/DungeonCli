ba4e61ec5178f76b08254aa25071e25b10b291cf
Daniel Pavela <daniel.pavela@gmail.com>
2020-05-29 16:10:47 +1000

    Forgot to set a var..

--- DungeonCli.py
+++ DungeonCli.py
@@ -387,7 +387,7 @@ def bossSuccess():
 	" the rebellion was dismantled, letting the town recover!\n" + Style.RESET_ALL)
 	time.sleep(2)
 
-	DGText.printspeed
+	DGText.printspeed = 0.008
 	creditScreen()
 	time.sleep(2)
 	DGExit()
