11b3d71a74176de9019d37fe59af527a2fba4373
Xenthio <thegooster88@gmail.com>
2020-06-17 13:12:03 +1000

    comment out self promotions, looks bad...

--- DungeonCli.py
+++ DungeonCli.py
@@ -1463,12 +1463,12 @@ def creditScreen():
 
 	time.sleep(1)
 	print("----".center(width, ' '))
-	DGText.printScan("Self Promotion from Sine:".center(width,' '))
-	DGText.printScan(("Youtube: " + Fore.CYAN + "https://www.youtube.com/channel/UCAK1pG_qcgqJd9d7BnayRtg" +
-	Style.RESET_ALL).center(b - 1,' ')) # SELF PROMO LOOLLL
-	DGText.printScan("Self Promotion from AsianPotato77:".center(b - 1,' '))
-	DGText.printScan(("SoundCloud:" + Fore.CYAN + "https://soundcloud.com/s-z-174676229").center(b - 1,' ') + Style.RESET_ALL)
-
+	#DGText.printScan("Self Promotion from Sine:".center(width,' '))
+	#DGText.printScan(("Youtube: " + Fore.CYAN + "https://www.youtube.com/channel/UCAK1pG_qcgqJd9d7BnayRtg" +
+	#Style.RESET_ALL).center(b - 1,' ')) # SELF PROMO LOOLLL
+	#DGText.printScan("Self Promotion from AsianPotato77:".center(b - 1,' '))
+	#DGText.printScan(("SoundCloud:" + Fore.CYAN + "https://soundcloud.com/s-z-174676229").center(b - 1,' ') + Style.RESET_ALL)
+	# STOP THE SELF PROMOTIONS! IT LOOKS DUMB! ALSO IM GONNA DO THE STORY CHANGE SOON I KEEP FORGETTING!!
 
 	print("-".center(width,'-'))
 	DGText.printScan(" ")
