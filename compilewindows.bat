e4695f6ce694936e5e237b1ceeacc21633dec4cd
Daniel Pavela <daniel.pavela@gmail.com>
2020-07-05 15:21:15 +1000

    Fix compiling in Windows

--- compilewindows.bat
+++ compilewindows.bat
@@ -1,5 +1,5 @@
 md bin.build
-python --standalone --remove-output --output-dir=bin.build --file-reference-choice=runtime DungeonCli.py
+python -m nuitka --standalone --remove-output --output-dir=bin.build --file-reference-choice=runtime DungeonCli.py
 copy .\Music .\bin.build\DungeonCli.dist\
 copy .\Sounds .\bin.build\DungeonCli.dist\
 echo the build is in .\bin.build\DungeonCli.dist\
