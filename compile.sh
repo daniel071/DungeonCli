4b0443e4e4b21f6bfe7d41c3511a9a8f186088a2
Daniel Pavela <daniel.pavela@gmail.com>
2020-07-07 22:03:04 +1000

    Made zip command recursive

--- compile.sh
+++ compile.sh
@@ -3,15 +3,15 @@
 mkdir bin.build
 python3 -m nuitka --standalone --remove-output --show-progress --show-scons --output-dir=bin.build --file-reference-choice=runtime DungeonCli.py
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	mv ./DungeonCli.bin ./bin.build/DungeonCli_osx
+	cp ./DungeonCli.bin ./bin.build/DungeonCli_osx
 	sudo codesign -f -s - ./bin.build/DungeonCli.dist/Python
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-	mv ./DungeonCli.bin ./bin.build/DungeonCli_linux
+	cp ./DungeonCli.bin ./bin.build/DungeonCli_linux
 else
-	mv ./DungeonCli.bin ./bin.build/DungeonCli
+	cp ./DungeonCli.bin ./bin.build/DungeonCli
 fi
 
 cp -r ./Music ./bin.build/DungeonCli.dist/
 cp -r ./Sounds ./bin.build/DungeonCli.dist/
 
-zip DungeonCli.zip ./bin.build/DungeonCli.dist/
+zip -r DungeonCli.zip ./bin.build/DungeonCli.dist/
