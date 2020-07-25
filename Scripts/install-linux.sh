7a41e50280a66aaff201a2160b913cb10ccdb7bc
Daniel Pavela <daniel.pavela@gmail.com>
2020-07-25 13:33:36 +1000

    Implement basic linux installation script

---
+++ Scripts/install-linux.sh
@@ -0,0 +1,5 @@
+mkdir ~/.dungeoncli/
+wget -O ~/.dungeoncli/dungeoncli.zip http://pavela.net:3000/attachments/9ba2e156-5732-4357-a641-269c25741f27
+wget -O ~/.dungeoncli/dungeoncli.png http://pavela.net:3000/Daniel/DungeonCli/raw/branch/master/Images/Logos/stableTerminal.png
+unzip ~/.dungeoncli/dungeoncli.zip -d ~/.dungeoncli/dungeoncli
+chmod +x ~/.dungeoncli/dungeoncli/DungeonCli-linux-v0.6.0/DungeonCli
