c362babeed687372a5fa4cab68e74cfd805a2733
Daniel Pavela <daniel.pavela@gmail.com>
2020-07-25 12:35:21 +1000

    Improve updater

--- Engine/DGUpdate.py
+++ Engine/DGUpdate.py
@@ -16,10 +16,14 @@ def update():
 	if currentOS == "windows":
 		DGText.printScan(DGText.rip + "Currently, only Linux and macOS support the automatic updater.\n")
 	else:
-		DGText.printScan(DGText.loading + "Downloading new update")
+		DGText.printScan(DGText.loading + "Downloading new update (1/2)")
 		print(Style.RESET_ALL)
-		os.system("wget -O ./dungeoncli http://pavela.net:3000/attachments/9ba2e156-5732-4357-a641-269c25741f27")
-		os.system("chmod +x ./dungeoncli")
-		DGText.printScan(DGText.success + "Update complete! Press enter to restart.")
+		os.system("wget -O ./dungeoncli.zip http://pavela.net:3000/attachments/9ba2e156-5732-4357-a641-269c25741f27")
+
+		DGText.printScan(DGText.loading + "Extracting new update (2/2)")
+		print(Style.RESET_ALL)
+		os.system("unzip ./dungeoncli.zip -d ./dungeoncli/")
+
+		DGText.printScan(DGText.success + "\nUpdate complete! Press enter to restart.")
 		input("")
 		DGMain.DGExit()
