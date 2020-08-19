7643742c7071236b10040c48c2230a27d3c7d4ac
Daniel Pavela <daniel.pavela@gmail.com>
2020-08-19 12:10:12 +1000

    Update links in script

--- Scripts/install-linux.sh
+++ Scripts/install-linux.sh
@@ -2,9 +2,9 @@
 
 mkdir ~/.dungeoncli/
 
-wget -O ~/.dungeoncli/dungeoncli.zip http://pavela.net:3000/attachments/9ba2e156-5732-4357-a641-269c25741f27
-wget -nv -O ~/.dungeoncli/dungeoncli.png http://pavela.net:3000/Daniel/DungeonCli/raw/branch/master/Images/Logos/stableTerminal.png
-wget -nv -O ~/.dungeoncli/dungeoncli_old.desktop http://pavela.net:3000/Daniel/DungeonCli/raw/branch/master/Scripts/dungeonCli.desktop
+wget -O ~/.dungeoncli/dungeoncli.zip https://git.pavela.net/attachments/9ba2e156-5732-4357-a641-269c25741f27
+wget -nv -O ~/.dungeoncli/dungeoncli.png https://git.pavela.net/Daniel/DungeonCli/raw/branch/master/Images/Logos/stableTerminal.png
+wget -nv -O ~/.dungeoncli/dungeoncli_old.desktop https://git.pavela.net/Daniel/DungeonCli/raw/branch/master/Scripts/dungeonCli.desktop
 
 unzip ~/.dungeoncli/dungeoncli.zip -d ~/.dungeoncli/dungeoncli
 chmod +x ~/.dungeoncli/dungeoncli/DungeonCli-linux-v0.6.0/DungeonCli
