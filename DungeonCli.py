147b30b3af36b65eb4dc4d43ba719300c3e46ffc
Daniel <daniel.pavela@gmail.com>
2022-02-08 09:29:41 +1100

    Various import changes

--- DungeonCli.py
+++ DungeonCli.py
@@ -13,7 +13,6 @@ import time
 import random
 import os
 import sys
-import readline
 from src import richPrecense
 from src import multiplayer
 from Game.Engine import *
@@ -38,6 +37,12 @@ from threading import Lock
 
 
 from colorama import init  # type: ignore
+operatingsystem = detect_system()
+
+# TODO: Improve requirements.txt to include all depends
+# Simpleaudio requires visual studio on windows?
+if not operatingsystem == 'windows':
+	import readline
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -1286,9 +1291,6 @@ def main():
 
 	else:
 		invalidCommand()
-
-
-operatingsystem = detect_system()
 
 if __name__ == '__main__':
 
